54b80a4a267633240c9747fc046e0a2563d0e86b
Akira Laine <akira_laine@outlook.com>
2019-02-23 14:51:23 +1100

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const RSS = require('rss')
+const fs = require('fs')
+const path = require('path')
+const camelcase = require('camelcase-keys');
+
+module.exports = function (api, options) {
+  api.beforeBuild(({ store }) => {
+    const feed = new RSS(options.feedOptions)
+    const { collection } = store.getContentType(options.contentTypeName)
+
+    collection.data.forEach(item => {
+      feed.item(options.feedItemOptions(camelcase(item), { deep: true }))
+    })
+  
+    fs.writeFileSync(path.resolve(process.cwd(), 'static/rss.xml'), feed.xml())
+  })
+}
