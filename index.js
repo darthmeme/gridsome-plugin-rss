39e8f31f81017770834c1a01dbd600cc7618722c
Ian Beck <ian@onecrayon.com>
2019-03-30 13:09:39 -0700

    Rewrote RSS generation logic to use `api.afterBuild()`
    This ensures that the RSS is only built into the distribution directory instead of living statically on disk.
    
    Rewrite includes the following changes:
    
    - **BREAKING CHANGE**: `siteUrl` *must* be defined in `gridsome.config.js`, or plug-in will exit with an error
    - **BREAKING CHANGE**: custom `options.output.dir` input is *no longer supported* and will result in plug-in exiting with an error
    - NEW: `options.outputPath` to specify where the RSS file will live in the built site (defaults to `/rss.xml`)
    - NEW: Default function provided for `options.feedItemOptions`, using standard `NodeType` attributes and automatically generates the URL based on `node.path`. Additionally, it will automatically convert relative paths within HTML content into absolute URLs including site scheme and host.
    - CHANGED: `options.feedItemOptions` function now accepts a second `siteUrl` parameter
    - CHANGED: `options.contentTypeName` is now explicitly required, and plug-in will exit with an error if it is missing
    - CHANGED: Build RSS during `api.afterBuild()`, similar to the first-party `@gridsome/plugin-sitemap`
    - CHANGED: Required properties for `options.feedOptions` are automatically gathered from existing data if they are not present (defaults to `siteName` and `siteUrl` from `gridsome.config.js`)
    - CHANGED: Basic defaults provided for all required options
    - CHANGED: Uses `fs-extra` instead of default Node `fs` module for writing RSS file

--- index.js
+++ index.js
@@ -1,33 +1,65 @@
 const RSS = require('rss')
-const fs = require('fs')
+const fs = require('fs-extra')
 const path = require('path')
+const url = require('url')
+
+function absolutizeUrls (html, siteUrl) {
+  // Currently playing it conservative and only modifying things that are explicitly relative URLs
+  const relativeRefs = /(href|src)=("|')((?=\.{1,2}\/|\/).+?)\2/gi
+  return html.replace(relativeRefs, (_, attribute, quote, relUrl) => {
+    return [attribute, '=', quote, new url.URL(relUrl, siteUrl).href, quote].join('')
+  })
+}
 
 module.exports = function (api, options) {
-  api.beforeBuild(({ store }) => {
-    const feed = new RSS(options.feedOptions)
+  api.afterBuild(({ config }) => {
+    if (!config.siteUrl) {
+      throw new Error('RSS plugin is missing required siteUrl config.')
+    }
+    if (!options.contentTypeName) {
+      throw new Error('RSS plugin is missing required `options.contentTypeName` setting.')
+    }
+    // Note new breaking change, for those using a custom output directory
+    if (options.output && options.output.dir !== './static') {
+      throw new Error('RSS plugin no longer supports custom `options.output.dir` setting.')
+    }
+
+    const store = api.store
+    const pathPrefix = config.pathPrefix !== '/' ? config.pathPrefix : ''
+    const siteUrl = new url.URL(pathPrefix, options.feedOptions.site_url || config.siteUrl).href
+    // Backwards compatibility for the original "build it in /static" approach
+    const oldOutputName = (options.output && options.output.name) || null
+    let outputPath = oldOutputName || options.outputPath
+    outputPath = outputPath.endsWith('.xml') ? outputPath : `${outputPath}.xml`
+    const feedOptions = {
+      ...options.feedOptions,
+      title: options.feedOptions.title || config.siteName,
+      site_url: siteUrl,
+      feed_url: options.feedOptions.feed_url || new url.URL(outputPath, siteUrl).href
+    }
+
+    const feed = new RSS(feedOptions)
     const { collection } = store.getContentType(options.contentTypeName)
 
     collection.data.forEach(item => {
-      feed.item(options.feedItemOptions(item))
+      feed.item(options.feedItemOptions(item, siteUrl))
     })
 
-    const output = {
-      dir: './static',
-      name: 'rss.xml',
-      ...options.output
-    }
-
-    const outputPath = path.resolve(process.cwd(), output.dir)
-    const outputPathExists = fs.existsSync(outputPath)
-    const fileName = output.name.endsWith('.xml')
-      ? output.name
-      : `${output.name}.xml`
-
-    if (outputPathExists) {
-      fs.writeFileSync(path.resolve(process.cwd(), output.dir, fileName), feed.xml())
-    } else {
-      fs.mkdirSync(outputPath)
-      fs.writeFileSync(path.resolve(process.cwd(), output.dir, fileName), feed.xml())
-    }
+    filePath = path.join(config.outDir, outputPath)
+    return fs.outputFile(filePath, feed.xml())
   })
 }
+
+module.exports.defaultOptions = () => ({
+  feedOptions: {
+    title: null,
+    site_url: null
+  },
+  feedItemOptions: (node, siteUrl) => ({
+    title: node.title,
+    date: node.date,
+    description: absolutizeUrls(node.content, siteUrl),
+    url: new url.URL(node.path, siteUrl).href
+  }),
+  outputPath: '/rss.xml'
+})
